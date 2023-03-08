d85bbff86e0d0f1b947a61d59553e5144c81d8ef
chodkows <chodkows@gmail.com>
2023-03-08 19:29:51 +0100

    Send client to barber

--- src/main.rs
+++ src/main.rs
@@ -9,43 +9,55 @@ use std::{
 
 const HAIR_CUT_TIME: Duration = Duration::from_secs(3);
 
-struct BarberShop {
-    shop_capacity: usize,
-    hair_cut_duration: Duration,
-    number_of_barbers: usize,
-    open: bool,
-}
-
-impl BarberShop {
-    fn new(
-        &self,
-        barber: String,
-        client_channel: Receiver<String>,
-        barbers_done: Sender<bool>,
-    ) -> JoinHandle<()> {
-        let rx = Arc::new(Mutex::new(client_channel));
-        let rx_clone = Arc::clone(&rx);
-        thread::spawn(move || {
-            println!("{} goes to the waiting room and check for clients", barber);
-            rx_clone
-                .lock()
-                .ok()
-                .unwrap()
-                .recv()
-                .into_iter()
-                .for_each(|client| {
-                    println!("{} is cutting {}'s hair", barber, client);
-                    thread::sleep(HAIR_CUT_TIME);
-                    println!("{} is finnished cutting {}'s hair", barber, client);
-                });
-            barbers_done
-                .send(true)
-                .ok()
-                .expect("Unable to send to barbers_done channel");
-        })
-    }
+fn barber(
+    barber: String,
+    client_channel: Arc<Mutex<Receiver<String>>>,
+    barbers_done: Sender<bool>,
+) -> JoinHandle<()> {
+    thread::spawn(move || {
+        println!("{} goes to the waiting room and check for clients", barber);
+        client_channel
+            .lock()
+            .ok()
+            .unwrap()
+            .recv()
+            .into_iter()
+            .for_each(|client| {
+                println!("{} is cutting {}'s hair", barber, client);
+                thread::sleep(HAIR_CUT_TIME);
+                println!("{} is finnished cutting {}'s hair", barber, client);
+            });
+        barbers_done
+            .send(true)
+            .ok()
+            .expect("Unable to send to barbers_done channel");
+    })
 }
 
 fn main() {
-    println!("Hello, world!");
+    let (client_tx, client_rx) = channel::<String>();
+    let (barber_tx, barber_rx) = channel();
+    let client_rx = Arc::new(Mutex::new(client_rx));
+    let mut handles = Vec::new();
+
+    for i in 0..1 {
+        let handle = barber(
+            format!("Barber{}", i),
+            Arc::clone(&client_rx),
+            barber_tx.clone(),
+        );
+        handles.push(handle);
+    }
+
+    for i in 0..100 {
+        client_tx
+            .send(format!("Client{}", i))
+            .ok()
+            .expect("Unable to send client");
+        thread::sleep(Duration::from_secs(1));
+    }
+
+    handles
+        .into_iter()
+        .for_each(|handle| handle.join().ok().unwrap());
 }
